97d003619f7eae16a44940b0545e80e1bc967b1b
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-11-17 14:16:22 +0330

    Add project files.

---
+++ WCFServiceApplication/CalculatorService.svc.cs
@@ -0,0 +1,16 @@
+﻿namespace WCFServiceApplication
+{
+    // This class implements the service contract defined in IService.cs
+    public class CalculatorService : ICalculatorService
+    {
+        public int Add(int a, int b)
+        {
+            return (a + b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return (a - b);
+        }
+    }
+}
---
+++ WCFServiceApplication/ICalculatorService.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+
+namespace WCFServiceApplication
+{
+    // Define a service contract
+    // ServiceContract is the short name for System.ServiceModel.ServiceContractAttribute
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        // Define an operation contract
+        // OperationContract is the short name for System.ServiceModel.OperationContractAttribute
+        [OperationContract]
+        int Add(int a, int b);
+
+        [OperationContract]
+        int Subtract(int a, int b);
+    }
+}
